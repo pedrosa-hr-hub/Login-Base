2c3550687d853bf6accdaf0fa4db5121484d0413
Pedro de Sá <pedro.trabalhos@outlook.com>
2023-06-30 22:33:31 -0300

    alter text to orientation to user

--- src/services/AuthBlock.js
+++ src/services/AuthBlock.js
@@ -2,6 +2,6 @@ export async function auth(req, res, next) {
     if (req.session.isAuthenticated) {
         next();
     } else {
-        res.status(401).json({ message: 'Não autenticado.' });
+        res.status(401).json({ message: 'Unauthenticated. Plase, verify' });
     }
 }
