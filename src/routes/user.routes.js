5608b6fd0ca50c53b7ffb32958adce5edaf7b7c0
Pedro de Sá <pedro.trabalhos@outlook.com>
2023-06-30 22:20:48 -0300

    route to send email

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,14 @@
-import { create, getUserbyEmail } from '../controller/user.controller';
+import {
+    create,
+    getUserbyEmail,
+    sendEmailtoUser,
+} from '../controller/user.controller';
 import { auth } from '../services/AuthBlock';
 
 const userRoutes = (app) => {
     app.post('/user', auth, create);
     app.get('/userEmail', getUserbyEmail);
+    app.get('/sendEmail', sendEmailtoUser);
 };
 
 export default userRoutes;
