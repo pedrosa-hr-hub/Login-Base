ce58a784e4f15a899e76b2a77a523c89f7668322
Pedro de Sá <pedro.trabalhos@outlook.com>
2023-07-02 18:14:04 -0300

    create route to acess updade

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,14 +2,16 @@ import {
     createUser,
     getUserbyEmail,
     sendEmailtoUser,
+    updateUser,
     test,
 } from '../controller/user.controller';
 import { auth } from '../services/AuthBlock';
 
 const userRoutes = (app) => {
     app.post('/user', auth, createUser);
-    app.get('/userEmail', getUserbyEmail);
+    app.get('/user', getUserbyEmail);
     app.get('/sendEmail', auth, sendEmailtoUser);
+    app.put('/user', auth, updateUser);
     app.get('/test', auth, test);
 };
 
