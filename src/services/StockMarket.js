ef1f7ea979b7e479e4b65c15f5d264bfe0a70bdd
Pedro de Sá <pedro.trabalhos@outlook.com>
2023-07-14 21:58:57 -0300

    create function to seach and response stock infos

--- src/services/StockMarket.js
+++ src/services/StockMarket.js
@@ -1,1 +1,26 @@
 const axios = require('axios');
+
+export async function catchStock(req, res) {
+    try {
+        if (req.ticker === null) {
+            res.status(400).json({
+                message: 'No ticker in bory, verify requisition',
+            });
+        } else {
+            await axios
+                .get(
+                    `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${req.ticker}&apikey=${process.env.ALPHA_KEY}`
+                )
+                .then((response_api) => {
+                    const data = response_api;
+
+                    res.status(200).json({ data: data });
+                })
+                .catch((error) => {
+                    res.status(400).json({ erro: error });
+                });
+        }
+    } catch (error) {
+        res.status(500).json({ error_msg: error });
+    }
+}
