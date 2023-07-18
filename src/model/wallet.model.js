ed0c84720f2a450badd7537b085d52a506f537cc
Pedro de Sá <pedro.trabalhos@outlook.com>
2023-07-17 21:29:10 -0300

    wallet model create

---
+++ src/model/wallet.model.js
@@ -0,0 +1,39 @@
+import { prisma } from '../services/prisma';
+
+export const create_Wallet = async (data) => {
+    const wallet = await prisma.wallet.create({
+        data,
+    });
+    return wallet;
+};
+
+export const find_WalletbyName = async (data) => {
+    const wallet = await prisma.wallet.findUnique({
+        where: {
+            name: data.name,
+        },
+    });
+    return wallet;
+};
+
+export const update_Wallet = async (data) => {
+    const wallet = await prisma.wallet.update({
+        where: {
+            id: data.id,
+        },
+        data: {
+            name: data.name,
+            idwallet: data.idwallet,
+        },
+    });
+    return wallet;
+};
+
+export const delete_Wallet = async (data) => {
+    const wallet = await prisma.wallet.delete({
+        where: {
+            id: data.id,
+        },
+    });
+    return wallet;
+};
